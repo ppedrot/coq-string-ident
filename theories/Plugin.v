d5cd40dfa5c991280d904834d6bc9aa902492a7b
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2020-02-10 09:12:36 +0100

    Port to 8.10.

--- theories/Plugin.v
+++ theories/Plugin.v
@@ -1,3 +1,9 @@
 Require String.
 
+Register String.EmptyString as stringident.nil.
+Register String.String as stringident.cons.
+Register Ascii.Ascii as stringident.ascii.
+Register Datatypes.true as stringident.bit1.
+Register Datatypes.false as stringident.bit0.
+
 Declare ML Module "tacstringident".
